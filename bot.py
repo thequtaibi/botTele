410332a65cdb5d6eb237c73ba818f2d323cf2526
thequtaibi <127546458+thequtaibi@users.noreply.github.com>
2024-06-10 09:03:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,26 +5,41 @@ import os
 
 TOKEN = '7252779471:AAF6zpHOJm4PjIcv8qNQV11Ey74j8wqeOXA'  # تأكد من وضع التوكن الصحيح هنا
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('حياك الله اخي المحارب ارسل الرابط للبداء ⚔️')
 
-def download_video(url):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        'حياك الله اخي المحارب ارسل الرابط للبداء ⚔️\nيمكنك إرسال روابط تيك توك أو يوتيوب واختيار الجودة المطلوبة (مثل 720p) أو دعها فارغة للحصول على الجودة الافضل.')
+
+
+def download_video(url, quality=None):
     ydl_opts = {
         'outtmpl': 'video.%(ext)s',
-        'format': 'bestvideo+bestaudio/best',
         'merge_output_format': 'mp4',
         'nocheckcertificate': True  # تعطيل التحقق من الشهادات SSL
     }
+
+    if quality:
+        ydl_opts['format'] = f'bestvideo[height<={quality}]+bestaudio/best[height<={quality}]'
+    else:
+        ydl_opts['format'] = 'bestvideo+bestaudio/best'
+
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(url, download=True)
         video_filename = ydl.prepare_filename(info_dict)
+
     return video_filename
 
+
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    url = update.message.text
-    if any(site in url for site in ["tiktok.com"]):
+    message = update.message.text.split()
+    url = message[0]
+    quality = None
+    if len(message) > 1:
+        quality = message[1]
+
+    if any(site in url for site in ["tiktok.com", "youtube.com", "youtu.be"]):
         try:
-            video_file = download_video(url)
+            video_file = download_video(url, quality)
             with open(video_file, 'rb') as video:
                 await update.message.reply_video(video=video)
             os.remove(video_file)  # احذف الملف بعد الإرسال لتوفير المساحة
@@ -32,7 +47,8 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await update.message.reply_text('يوجد مشكلة نعتذر أية المحارب 😞.')
             print(f"Error: {e}")
     else:
-        await update.message.reply_text('فقط تيك توك ✋🏻')
+        await update.message.reply_text('فقط روابط تيك توك و يوتيوب ✋🏻')
+
 
 def main():
     application = ApplicationBuilder().token(TOKEN).build()
@@ -41,6 +57,7 @@ def main():
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     application.run_polling()
+
 
 if __name__ == '__main__':
     main()
